01dce35258b3f78f5e975de7e65fb3a5a40c53fc
grazianodinocca <39311943+grazianodinocca@users.noreply.github.com>
2018-05-31 17:55:06 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -46,40 +46,44 @@ def processRequest(req):
     
     result = req.get("result")
     parameters = result.get("parameters")
-    city = parameters.get("geo-city")
-    outfit = result.get("action")
-    print(outfit)
-    observation = owm.weather_at_place(city)
-    w = observation.get_weather()
-    latlon_res = observation.get_location()
-    print(w.get_status())
-    description = w.get_detailed_status()
-    description = description[0].upper()+description[1:]
-    lat=str(latlon_res.get_lat())
-    lon=str(latlon_res.get_lon())
-    wind_res=w.get_wind()
-    wind_speed=str(int(round(wind_res.get('speed'))))
-    wind=str(processWindDegrees(wind_res.get('deg')))
+    
+    action = result.get("action")
+    if action=="weather"
+        print("tempo")
+        city = parameters.get("geo-city")
+        observation = owm.weather_at_place(city)
+        w = observation.get_weather()
+        latlon_res = observation.get_location()
+        print(w.get_status())
+        description = w.get_detailed_status()
+        description = description[0].upper()+description[1:]
+        lat=str(latlon_res.get_lat())
+        lon=str(latlon_res.get_lon())
+        wind_res=w.get_wind()
+        wind_speed=str(int(round(wind_res.get('speed'))))
+        wind=str(processWindDegrees(wind_res.get('deg')))
         
     
-    humidity=str(w.get_humidity())
+        humidity=str(w.get_humidity())
     
-    celsius_result=w.get_temperature('celsius')
-    temp=str(int(round(celsius_result.get('temp'))))
-    temp_min_celsius=str(celsius_result.get('temp_min'))
-    temp_max_celsius=str(celsius_result.get('temp_max'))
+        celsius_result=w.get_temperature('celsius')
+        temp=str(int(round(celsius_result.get('temp'))))
+        temp_min_celsius=str(celsius_result.get('temp_min'))
+        temp_max_celsius=str(celsius_result.get('temp_max'))
     
-    fahrenheit_result=w.get_temperature('fahrenheit')
-    temp_min_fahrenheit=str(fahrenheit_result.get('temp_min'))
-    temp_max_fahrenheit=str(fahrenheit_result.get('temp_max'))
-    speech = description + " a " + city + " con "+temp+"°C. Umidità al "+humidity+"% con venti da " +wind+ " a "+wind_speed+" km/h."
+        fahrenheit_result=w.get_temperature('fahrenheit')
+        temp_min_fahrenheit=str(fahrenheit_result.get('temp_min'))
+        temp_max_fahrenheit=str(fahrenheit_result.get('temp_max'))
+        speech = description + " a " + city + " con "+temp+"°C. Umidità al "+humidity+"% con venti da " +wind+ " a "+wind_speed+" km/h."
     
-    return {
-        "speech": speech,
-        "displayText": speech,
-        "source": "dialogflow-weather-by-dinocca-graziano"
-        }
-    
+        return {
+            "speech": speech,
+            "displayText": speech,
+            "source": "dialogflow-weather-by-dinocca-graziano"
+            }
+    if action=="weather.outfit"
+        print("vestiario")
+        
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
